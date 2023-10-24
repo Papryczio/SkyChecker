73c6fd4b6a140ad41ac91b71a90169b6f6991b37
Sutkorwij <patrix800@gmail.com>
2023-10-24 21:52:49 +0200

    [TASK] Fixed dates added

--- src/script.py
+++ src/script.py
@@ -1,6 +1,5 @@
 import requests
 from requests.structures import CaseInsensitiveDict
-import json
 from datetime import datetime, timedelta
 import os
 import smtplib
@@ -9,6 +8,21 @@ from email.message import EmailMessage
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 import logging
+
+def main():
+    db = CLIENT.skyChecker.Configuration
+    for config in db.find():
+        logging.info("========================================================")
+        logging.info(f"Processing flight: {config.get('header')}")
+        apiQuery = createAPIquery(config)
+        response = getAPIresponse(apiQuery)
+        searchForFlightsFittingCriteria(config, response)
+
+# ===================================================
+#                 CONFIGURATION
+# ===================================================
+
+logging.basicConfig(level=logging.INFO)
 
 # Skyscanner API config
 URL = "https://partners.api.skyscanner.net/apiservices/v3/flights/indicative/search"
@@ -23,41 +37,36 @@ MONGO_PASSWORD      = os.getenv('MONGO_PASSWORD')
 context             = ssl.create_default_context()
 
 # Configuring database
-uri = "mongodb+srv://skyChecker:" + str(MONGO_PASSWORD) + "@skychecker.qjthns8.mongodb.net/?retryWrites=true&w=majority"
-client = MongoClient(uri, server_api=ServerApi('1'))
+URI = "mongodb+srv://skyChecker:" + str(MONGO_PASSWORD) + "@skychecker.qjthns8.mongodb.net/?retryWrites=true&w=majority"
+CLIENT = MongoClient(URI, server_api=ServerApi('1'))
 
+# Configuration of the currency for flight search
 MARKET      = "PL"
 LOCALE      = "pl-PL"
 CURRENCY    = "PLN"
 
-# Getting all configured flights configurations.
-def execute():
-    db = client.skyChecker.Configuration
-    for config in db.find():
-        lookForFlightOffers(config)
-
-def lookForFlightOffers(config):
-    print("\n========================================================")
-    print(f"Processing flight: {config.get('header')}")
-    apiQuery = createAPIquery(config)
-    getAPIresponse(config, apiQuery)
+# ===================================================
+#         FETCHING DATA FROM SKYSCANNER API
+# ===================================================
 
 # Curl command parsing.
 def createAPIquery(config):
     query = '{"query":{'
-    query += '"market":"' + MARKET + '","locale":"' + LOCALE + '","currency":"' + CURRENCY + '","dateTimeGroupingType":"DATE_TIME_GROUPING_TYPE_BY_DATE","queryLegs":['
-    query = insertAirports(config, query, False)
-    query = insertDates(config, query, False)
+    query += '"market":"' + MARKET + '","locale":"' + LOCALE + '","currency":"' + CURRENCY + '"'
+    if (config.get("isFixed") != "True"):
+        query += ',"dateTimeGroupingType":"DATE_TIME_GROUPING_TYPE_BY_DATE"'
+    query += ',"queryLegs":['
+    query = insertAirportsToQuery(config, query, False)
+    query = insertDatesToQuery(config, query, False)
         
     if config.get("return").lower() == "true":
         query += ','
-        query = insertAirports(config, query, True)
-        query = insertDates(config, query, True)
+        query = insertAirportsToQuery(config, query, True)
+        query = insertDatesToQuery(config, query, True)
         query += ']}}'
-    print(query)
     return query
 
-def insertAirports(config, query, isReturn):
+def insertAirportsToQuery(config, query, isReturn):
         originEntityID =        config.get("originAirportEntityID") or None 
         originIATA =            config.get("originAirportIATA") or None
         destinationEntityID =   config.get("destinationAirportEntityID") or None
@@ -77,18 +86,26 @@ def insertAirports(config, query, isReturn):
         
         return query
 
-def insertDates(config, query, isReturn):
-    dayFrom =   str(config.get("dateFromReturn").get("day"))    if isReturn else    str(config["dateFrom"]["day"])
-    dayTo =     str(config.get("dateToReturn").get("day"))      if isReturn else    str(config["dateTo"]["day"])
-    monthFrom = str(config.get("dateFromReturn").get("month"))  if isReturn else    str(config["dateFrom"]["month"])
-    monthTo =   str(config.get("dateToReturn").get("month"))    if isReturn else    str(config["dateTo"]["month"])
-    yearFrom =  str(config.get("dateFromReturn").get("year"))   if isReturn else    str(config["dateFrom"]["year"])
-    yearTo =    str(config.get("dateToReturn").get("year"))     if isReturn else    str(config["dateTo"]["year"])
+def insertDatesToQuery(config, query, isReturn):
+    if (config.get("isFixed") == "True"):
+        # Fixed dates (can specify days)
+        day =   str(config.get("dateReturn").get("day"))    if isReturn else    str(config["date"]["day"])
+        month = str(config.get("dateReturn").get("month"))  if isReturn else    str(config["date"]["month"])
+        year =  str(config.get("dateReturn").get("year"))   if isReturn else    str(config["date"]["year"])
 
-    query += '"dateRange":{"startDate":{"day":' + dayFrom + ',"month":' + monthFrom + ',"year":' + yearFrom + '},"endDate":{"day":' + dayTo + ',"month":' + monthTo + ',"year":' + yearTo + '}}}'
+        query += '"fixedDate":{"day":' + day + ',"month":' + month + ',"year":' + year + '}}'
+    else:
+        # Not fixed dated (whole month only, cannot specify days)
+        monthFrom = str(config.get("dateFromReturn").get("month"))  if isReturn else    str(config["dateFrom"]["month"])
+        monthTo =   str(config.get("dateToReturn").get("month"))    if isReturn else    str(config["dateTo"]["month"])
+        yearFrom =  str(config.get("dateFromReturn").get("year"))   if isReturn else    str(config["dateFrom"]["year"])
+        yearTo =    str(config.get("dateToReturn").get("year"))     if isReturn else    str(config["dateTo"]["year"])
+
+        query += '"dateRange":{"startDate":{"month":' + monthFrom + ',"year":' + yearFrom + '},"endDate":{"month":' + monthTo + ',"year":' + yearTo + '}}}'
+        
     return query
 
-def getAPIresponse(config, query):
+def getAPIresponse(query):
     try:
         response = requests.post(URL, headers=headers, data=query)
         if response.status_code != 200:
@@ -96,77 +113,103 @@ def getAPIresponse(config, query):
             logging.debug(query)
             logging.debug(response)
         
-        flightsInfo = response.json()['content']['results']['quotes']
-        
-        minPrice = 99999999
-        bestFittedFlight = ""
-        
-        for flightCode in flightsInfo:
-            flight = flightsInfo.get(flightCode)
+        return response.json()['content']['results']['quotes']
 
-            # In case the flight is return - check max and min days between flights.
-            if config.get("return").lower() == "true":
-                date = str(flight['outboundLeg']['departureDateTime']['year']) + "-"
-                date += str(flight['outboundLeg']['departureDateTime']['month']) + "-"
-                date += str(flight['outboundLeg']['departureDateTime']['day'])
-                outboundDate = datetime.strptime(date, "%Y-%m-%d")
-
-                date = str(flight['inboundLeg']['departureDateTime']['year']) + "-"
-                date += str(flight['inboundLeg']['departureDateTime']['month']) + "-"
-                date += str(flight['inboundLeg']['departureDateTime']['day'])
-                inboundDate = datetime.strptime(date, "%Y-%m-%d")
-
-                difference = inboundDate - outboundDate
-                minDays = timedelta(days=config.get("daysMinimum"))
-                maxDays = timedelta(days=config.get("daysMaximum"))
-
-                if(difference < minDays or difference > maxDays):
-                    continue
-
-            # Check if the flight is direct or not.
-            if (str(flight['isDirect']).lower() != config.get("onlyDirectFlights").lower()):
-                continue
-
-            # Check if the flight is cheaper then the cheapest one yet found.
-            if(int(flight['minPrice']['amount']) < minPrice):
-                bestFittedFlight = flight
-                minPrice = int(flight['minPrice']['amount'])
-
-        # Skipping if there are no flights at all, triggering mail if threshold met.
-        if minPrice == 999999999:
-            logging.info("No flights fitted given criteria.")
-            logging.info("========================================================\n")
-            return
-        elif minPrice > config.get("priceNotification"):
-            logging.info(f"No flights found in the given price (up to {config.get('priceNotification')} {CURRENCY}). The cheapest flight costs {minPrice} {CURRENCY}.")
-        else:
-            print(f"Flight fits the criteria, the price is {minPrice}")
-            sendEmail(bestFittedFlight, config, minPrice)
-
-        logging.info(f"Is direct?:     {bestFittedFlight['isDirect']}")
-        logging.info(f"Departure from: {bestFittedFlight['outboundLeg']['originPlaceId']}")
-        logging.info(f"Arrival to:     {bestFittedFlight['outboundLeg']['destinationPlaceId']}")
-        logging.info(f"Departure date: {bestFittedFlight['outboundLeg']['departureDateTime']['year']}-{bestFittedFlight['outboundLeg']['departureDateTime']['month']}-{bestFittedFlight['outboundLeg']['departureDateTime']['day']}")
-        if config.get("return"):
-            logging.info(f"Return from:    {bestFittedFlight['inboundLeg']['originPlaceId']}")
-            logging.info(f"Return to:      {bestFittedFlight['inboundLeg']['destinationPlaceId']}")
-            logging.info(f"Return date:    {bestFittedFlight['inboundLeg']['departureDateTime']['year']}-{bestFittedFlight['inboundLeg']['departureDateTime']['month']}-{bestFittedFlight['inboundLeg']['departureDateTime']['day']}")
-        logging.info("========================================================\n")
-
-        insertIntoDatabase(config, bestFittedFlight, minPrice)
     except Exception as ex:
         logging.error(ex)
         logging.debug(query)
         logging.debug(response)
 
+# ===================================================
+#               API RESPONSE ANALYSIS
+# ===================================================
+
+def searchForFlightsFittingCriteria(config, flightsInfo):
+    minPrice = 99999999
+    bestFittedFlight = ""
+        
+    for flightCode in flightsInfo:
+        flight = flightsInfo.get(flightCode)
+
+        # In case the flight is return - check max and min days between flights.
+        if (config.get("return").lower() == "true" and config.get("isFixed") != "True"):
+            date = str(flight['outboundLeg']['departureDateTime']['year']) + "-"
+            date += str(flight['outboundLeg']['departureDateTime']['month']) + "-"
+            date += str(flight['outboundLeg']['departureDateTime']['day'])
+            outboundDate = datetime.strptime(date, "%Y-%m-%d")
+
+            date = str(flight['inboundLeg']['departureDateTime']['year']) + "-"
+            date += str(flight['inboundLeg']['departureDateTime']['month']) + "-"
+            date += str(flight['inboundLeg']['departureDateTime']['day'])
+            inboundDate = datetime.strptime(date, "%Y-%m-%d")
+
+            difference = inboundDate - outboundDate
+            minDays = timedelta(days=config.get("daysMinimum"))
+            maxDays = timedelta(days=config.get("daysMaximum"))
+
+            if(difference < minDays or difference > maxDays):
+                continue
+
+        # Check if the flight is direct or not.
+        if (str(flight['isDirect']).lower() != config.get("onlyDirectFlights").lower()):
+            continue  
+               
+        # Check if the flight is cheaper then the cheapest one yet found.
+        if(int(flight['minPrice']['amount']) < minPrice):
+            bestFittedFlight = flight
+            minPrice = int(flight['minPrice']['amount'])
+
+    # Skipping if there are no flights at all, triggering mail if threshold met.
+    if bestFittedFlight == "":
+        logging.info("No flights fitted given criteria.")
+        logging.info("========================================================")
+        return
+    else:
+        if minPrice > config.get("priceNotification"):
+            logging.info(f"No flights found in the given price (up to {config.get('priceNotification')} {CURRENCY}). The cheapest flight costs {minPrice} {CURRENCY}.")
+        else:
+            print(f"Flight fits the criteria, the price is {minPrice}")
+            sendEmail(bestFittedFlight, config, minPrice)
+        
+        logging.info(f"Is direct?:     {bestFittedFlight['isDirect']}")
+        logging.info(f"Departure from: {bestFittedFlight['outboundLeg']['originPlaceId']}")
+        logging.info(f"Arrival to:     {bestFittedFlight['outboundLeg']['destinationPlaceId']}")
+        logging.info(f"Departure date: {bestFittedFlight['outboundLeg']['departureDateTime']['year']}-{bestFittedFlight['outboundLeg']['departureDateTime']['month']}-{bestFittedFlight['outboundLeg']['departureDateTime']['day']}")
+            
+        if config.get("return"):
+            logging.info(f"Return from:    {bestFittedFlight['inboundLeg']['originPlaceId']}")
+            logging.info(f"Return to:      {bestFittedFlight['inboundLeg']['destinationPlaceId']}")
+            logging.info(f"Return date:    {bestFittedFlight['inboundLeg']['departureDateTime']['year']}-{bestFittedFlight['inboundLeg']['departureDateTime']['month']}-{bestFittedFlight['inboundLeg']['departureDateTime']['day']}")
+        logging.info("========================================================")
+            
+        insertIntoDatabase(config, bestFittedFlight, minPrice)
+
+# ===================================================
+#      DATABASE INSERT AND EMAIL NOTIFICATION
+# ===================================================
+
 def insertIntoDatabase(config, flight, minPrice):
     try:
-        db = client.skyChecker.flightData
-
-        insert = {"name": config.get("header"), "from": flight['outboundLeg']['originPlaceId'], "to": flight['outboundLeg']['destinationPlaceId'], "direct": flight['isDirect'], 
-            "departure": str(flight['outboundLeg']['departureDateTime']['year']) + "-" + str(flight['outboundLeg']['departureDateTime']['month']) + "-" + str(flight['outboundLeg']['departureDateTime']['day']),
-            "price": minPrice, "checkDate": datetime.now()}
+        db = CLIENT.skyChecker.flightData
         
+        db_name         = config.get("header")
+        db_from         = flight['outboundLeg']['originPlaceId']
+        db_to           = flight['outboundLeg']['destinationPlaceId']
+        db_direct       = flight['isDirect']
+        db_departure    = str(flight['outboundLeg']['departureDateTime']['year']) + "-" + str(flight['outboundLeg']['departureDateTime']['month']) + "-" + str(flight['outboundLeg']['departureDateTime']['day'])
+        db_price        = minPrice
+        db_checkDate    = datetime.now()
+
+        insert = {
+            "name": db_name,
+            "from": db_from,
+            "to": db_to,
+            "direct": db_direct, 
+            "departure": db_departure,
+            "price": db_price,
+            "checkDate": db_checkDate
+        }
+
         if config.get("return"):
             insert["return"] = str(flight['inboundLeg']['departureDateTime']['year']) + "-" + str(flight['inboundLeg']['departureDateTime']['month']) + "-" + str(flight['inboundLeg']['departureDateTime']['day'])
         
@@ -205,4 +248,9 @@ def sendEmail(flight, config, minPrice):
         smtp.login(EMAIL_SENDER, EMAIL_PASSWORD)
         smtp.sendmail(EMAIL_SENDER, emailAddress, em.as_string())
 
-execute()
+# ===================================================
+#                EXECUTE SCRIPT
+# ===================================================
+
+if __name__ == "__main__":
+    main()
